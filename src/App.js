ee3efeb7e33058965936a44483bac5c2c83afc53
808287 <prashant17699kumar@gmail.com>
2022-05-22 13:23:44 +0530

    react.js first app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './Component/Header/header';
+import Footer from './Component/Footer/footer';
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Footer/footer.jsx
@@ -0,0 +1,35 @@
+import './footer.css';
+function Footer(){
+    return(
+        <div className="bottom">
+            <h2>Summary</h2>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
+            mollit anim id est laborum.
+            </p>
+            <h2>Experience</h2>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
+            mollit anim id est laborum.
+            </p>
+            <h2>Contact</h2>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
+            mollit anim id est laborum.
+            </p>
+        </div>
+    );
+}
+export default Footer;
